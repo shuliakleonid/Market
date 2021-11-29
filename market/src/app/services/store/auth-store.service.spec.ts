57c4c32a2c5d0ed4aa9ad911d435a87d90fd06bb
l.shuliak <zeleny777@gmail.com>
2021-11-29 18:55:33 +0300

    add: test cover to login

--- market/src/app/services/store/auth-store.service.spec.ts
+++ market/src/app/services/store/auth-store.service.spec.ts
@@ -1,11 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { AuthStoreService } from './auth-store.service';
 
 describe('AuthStoreService', () => {
-  beforeEach(() => {
+  beforeEach(async( () => {
     TestBed.configureTestingModule({
       imports: [],
       providers: [AuthStoreService],
     });
-  });
+  }));
 });
